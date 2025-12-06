900e0d3627219e5e75128bd9e313575c9b6f25e6
DevPatata <anavaro.sala@gmail.com>
2025-12-06 01:08:57 +0100

    fifth

--- fourth/main.cpp
+++ fourth/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
+
+int checker(vector<string> rolls, int x, int y)
+{
+	int count = 0;
+	for (long long i = x - 1; i <= x + 1; i++)
+	{
+		for (long long j = y - 1; j <= y + 1; j++)
+		{
+			if(i < 0 || j < 0 ||
+				i >= rolls.size() || j >= rolls[x].size())
+				continue;
+			if(x == i && y == j)
+				continue;
+			if(rolls[i][j] == '@')
+				count++;
+		}
+	}
+	return (count < 4) ? 1 : 0;
+}
 
 int main(int argc, char **argv)
 {
@@ -15,17 +35,40 @@ int main(int argc, char **argv)
 
     string line;
     long long result = 0;
+    long long changed = 1;
+    vector<string> rolls;
+    vector<string> rollsaux;
 
 
     while (getline(file, line))
     {
         if (line.empty())
             continue;
-		result++;
-    	cout << "result: " << result << endl;
+		rolls.push_back(line);
         
     }
+	rollsaux = rolls;
 
+	while(changed != 0)
+	{
+		changed = 0;
+		rolls  = rollsaux;
+		for (size_t i = 0; i < rolls.size(); i++)
+		{
+			for (size_t j = 0; j < rolls[i].size(); j++)
+			{
+				if(rolls[i][j] == '@')
+				{
+					if(checker(rolls, i, j)){
+					result ++ ;
+					changed ++;
+					rollsaux[i][j] = '.';
+					cout << i << ","<< j << endl;}
+				}
+			}
+		}
+	}
+	
     cout << "result: " << result << endl;
     return 0;
 }
